edfae4c462162438e4bc1c0fc24a1aa5a2826a34
Apollo <apltmendoza@brc.pshs.edu.ph>
2026-09-13 16:53:35 +0800

    Update classRelationships.py

--- q1/OOPActPartIII/classRelationships.py
+++ q1/OOPActPartIII/classRelationships.py
@@ -1,97 +1,88 @@
 class Potion:
-def **init**(self, name, effect, strength, uses):
-self.name = name
-self.effect = effect
-self.strength = strength
-self.__uses = uses
+    def __init__(self, name, effect, strength, uses):
+        self.name = name
+        self.effect = effect
+        self.strength = strength
+        self.__uses = uses
 
-```
-def usePotion(self):
-    if self.__uses > 0:
-        self.__uses -= 1
-        print(self.name, "was used.")
-    else:
-        print("No uses left.")
+    def usePotion(self):
+        if self.__uses > 0:
+            self.__uses -= 1
+            print(self.name, "was used.")
+        else:
+            print("No uses left.")
 
-def increaseStrength(self, amount):
-    self.strength += amount
+    def increaseStrength(self, amount):
+        self.strength += amount
 
-def displayInfo(self):
-    print("Name:", self.name)
-    print("Effect:", self.effect)
-    print("Strength:", self.strength)
-    print("Uses:", self.__uses)
-```
+    def displayInfo(self):
+        print("Name:", self.name)
+        print("Effect:", self.effect)
+        print("Strength:", self.strength)
+        print("Uses:", self.__uses)
+
 
 class Tome:
-def **init**(self, title, author):
-self.title = title
-self.author = author
-self.contents = []
-self.is_open = False
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.contents = []
+        self.is_open = False
 
-```
-def open(self):
-    self.is_open = True
-    print(self.title, "has been opened.")
+    def open(self):
+        self.is_open = True
+        print(self.title, "has been opened.")
 
-def close(self):
-    self.is_open = False
-    print(self.title, "has been closed.")
+    def close(self):
+        self.is_open = False
+        print(self.title, "has been closed.")
 
-def addPotion(self, potion):
-    self.contents.append(potion)
-    print(potion.name, "was added to", self.title + ".")
+    def addPotion(self, potion):
+        self.contents.append(potion)
+        print(potion.name, "was added to", self.title + ".")
 
-def displayContents(self):
-    print("\nContents of", self.title + ":")
-    
-    if len(self.contents) == 0:
-        print("The Tome contains no potions.")
-    else:
-        for potion in self.contents:
-            print("-", potion.name)
-            print("  Effect:", potion.effect)
-            print("  Strength:", potion.strength)
-```
+    def displayContents(self):
+        print("\nContents of", self.title + ":")
 
-# ----------------------------------------------
+        if len(self.contents) == 0:
+            print("The Tome contains no potions.")
+        else:
+            for potion in self.contents:
+                print("-", potion.name)
+                print("  Effect:", potion.effect)
+                print("  Strength:", potion.strength)
 
-#= CREATE OBJECTS
 
-# -------------------------------------------
+# CREATE OBJECTS
 
 potion1 = Potion(
-"Phoenix Elixir",
-"Restores health",
-50,
-3
+    "Phoenix Elixir",
+    "Restores health",
+    50,
+    3
 )
 
 potion2 = Potion(
-"Frost Draught",
-"Slows enemies",
-30,
-5
+    "Frost Draught",
+    "Slows enemies",
+    30,
+    5
 )
 
 potion3 = Potion(
-"Shadow Tonic",
-"Improves stealth",
-40,
-4
+    "Shadow Tonic",
+    "Improves stealth",
+    40,
+    4
 )
 
 tome = Tome(
-"The Alchemist's Tome",
-"Eldrin"
+    "The Alchemist's Tome",
+    "Eldrin"
 )
 
-# -----------------------------------------------
 
 # BEFORE RELATIONSHIP
-
-# --------------------------------------------------
 
 print("--- BEFORE RELATIONSHIP ---")
 
@@ -111,11 +102,8 @@ print()
 
 potion3.displayInfo()
 
-# --------------------------------------------------
 
-# BUILDING RELATIONSHIPS
-
-# ----------------------------------------
+# BUILDING RELATIONSHIP
 
 print("\n--- BUILDING RELATIONSHIP ---")
 
@@ -123,11 +111,8 @@ tome.addPotion(potion1)
 tome.addPotion(potion2)
 tome.addPotion(potion3)
 
-# --------------------------------------------------
 
 # AFTER RELATIONSHIP
-
-# --------------------------------------------------
 
 print("\n--- AFTER RELATIONSHIP ---")
 
@@ -137,22 +122,16 @@ print("Number of potions:", len(tome.contents))
 
 tome.displayContents()
 
-# --------------------------------------------------
 
 # ACCESSING DATA THROUGH THE RELATIONSHIP
-
-# --------------------------------------------------
 
 print("\n--- ACCESSING DATA THROUGH RELATIONSHIP ---")
 
 for potion in tome.contents:
-print(potion.name, "has", potion.strength, "strength.")
+    print(potion.name, "has", potion.strength, "strength.")
 
-# --------------------------------------------------
 
 # USING A RELATED OBJECT THROUGH THE TOME
-
-# --------------------------------------------------
 
 print("\n--- USING A POTION THROUGH THE TOME ---")
 
@@ -162,11 +141,8 @@ print("\nUpdated Phoenix Elixir:")
 
 tome.contents[0].displayInfo()
 
-# --------------------------------------------------
 
 # TOME OPEN/CLOSE
-
-# --------------------------------------------------
 
 print("\n--- TOME STATUS ---")
 
